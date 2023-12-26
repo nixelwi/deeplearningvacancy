a7bad84b6b8b461937697e1cae482fd508beddb6
Artemy <150930596+nixelwi@users.noreply.github.com>
2023-12-26 19:04:19 +0600

    Update matching.py

--- demo/models/matching.py
+++ demo/models/matching.py
@@ -3,10 +3,10 @@ import pandas as pd
 from commands_faiss import embeded_id
 
 if __name__ == "__main__":
-    cv_csv = "./data/processed/res_prepared.csv"
-    vac_csv = "./data/processed/vac_prepared.csv"
-    cv_index_demo = "demo/data/cv_db.index"
-    vac_index_demo = "demo/data/vac_db.index"
+    cv_csv = "case2cv/data/processed/res_prepared.csv"
+    vac_csv = "case2cv/data/processed/vac_prepared.csv"
+    cv_index_demo = "case2cv/demo/demo_application/data/cv_db.index"
+    vac_index_demo = "case2cv/demo/demo_application/data/vac_db.index"
     vac_id = 7
     topn = 10
 
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     cv_data = pd.read_csv(cv_csv)
     vac_data = pd.read_csv(vac_csv)
 
-    vac_embed = get_embedding_by_id(vac_index, vac_id)
+    vac_embed = embeded_id(vac_index, vac_id)
     _, cv_ids = cv_index.search(vac_embed, topn)
 
     print("Для лучшего работодателя")
